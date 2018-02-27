7484ec70cebf6cd16894fc8a2e00614a4e1099ba
LAPTOP-L2GDHE5T\zykas <zykas1@gmail.com>
2018-02-27 22:32:40 +0200

    --commit-- first jump attempt

--- Assets/zykas/Scripts/PlayerMovement.cs
+++ Assets/zykas/Scripts/PlayerMovement.cs
@@ -5,10 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed = 10.0f;
-
+    bool canJump = true;
+    public float jumpForce=250;
 	void Update () {
         float translation = Input.GetAxis("Horizontal") * speed;
         translation *= Time.deltaTime;
         transform.Translate(translation, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            jump();
+        }
 	}
+
+    void jump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            GetComponent<Rigidbody>().AddForce(this.gameObject.transform.up * jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 0)
+        {
+            canJump=true;
+        }
+    }
 }
