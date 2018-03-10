170e480306ed69a576ed988cdd35a4b51492e774
LAPTOP-L2GDHE5T\zykas <zykas1@gmail.com>
2018-03-10 22:12:35 +0200

    --commit-- add death from tag "death" and slow from tag "abstacle"

---
+++ Assets/zykas/Scripts/PlayerInteraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    PlayerMovement playerMovement;
+    public float slowDuration = 5f;
+
+    void Start () {
+        playerMovement = GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Death")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+        if(hit.collider.tag == "obstacle")
+        {
+            Destroy(hit.gameObject);
+            StartCoroutine(slowTimeDuration());
+
+        }
+    }
+
+
+    IEnumerator slowTimeDuration()
+    {
+        float tempSpeed = playerMovement.speed;
+        playerMovement.speed = slowDuration;
+        yield return new WaitForSeconds( 2.0f);
+        playerMovement.speed = tempSpeed;
+    }
+}
