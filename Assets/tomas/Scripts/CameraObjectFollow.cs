6046f25f8b3d58ea727626b569106183345e9b97
potatoorgylt <gynejas13@gmail.com>
2018-03-12 01:28:21 +0200

    Advanced object follow. Now follows by min and max object distances.

--- Assets/tomas/Scripts/CameraObjectFollow.cs
+++ Assets/tomas/Scripts/CameraObjectFollow.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraObjectFollow : MonoBehaviour {
-    public float maxZoomDistance = 30.0f;
+    public float maxZoomOutDistance = 30.0f;
+    public float maxZoomInDistance = 10.0f;
     public float maxLastTileDistance = 30.0f;
+    public Vector3 cameraStaticPosition;
     private float cameraOffsetZ = 10.0f;
     float greatestDistance;
     float aspectRatio;
     float tanFov;
     Vector3 centerOfVectors;
     List<GameObject> players = new List<GameObject>();
+    float minVector;
+    float maxVector;
 
     WorldGeneration worldGeneration;
     GameObject generationObject;
@@ -38,23 +42,27 @@ public class CameraObjectFollow : MonoBehaviour {
 	void Update () {
         aspectRatio = Screen.width / Screen.height;
         greatestDistance = Distance(players, players.Count-1);
-        centerOfVectors = CenterOfVectors(players);
-
-        cameraOffsetZ = (greatestDistance / 2 / aspectRatio) / tanFov;
+        CenterOfVectors(players);
+        centerOfVectors.x = (minVector + maxVector) / 2;
+        Debug.Log("min x is: " + minVector + ", max x is: " + maxVector);
+        cameraOffsetZ = ((greatestDistance + cameraStaticPosition.x) / 2 / aspectRatio) / tanFov;
 
         if(centerOfVectors.x + maxLastTileDistance*(-1) >= worldGeneration.tileClone[0].transform.position.x)
         {
-            if (cameraOffsetZ <= maxZoomDistance)
-                transform.position = centerOfVectors + new Vector3(0, 0, cameraOffsetZ * (-1));
+            if (cameraOffsetZ <= maxZoomOutDistance)
+                transform.position = cameraStaticPosition + new Vector3(centerOfVectors.x, 0, cameraOffsetZ * (-1));
             else
-                transform.position = centerOfVectors + new Vector3(0, 0, maxZoomDistance * (-1));
+                transform.position = cameraStaticPosition + new Vector3(centerOfVectors.x, 0, maxZoomOutDistance * (-1));
+
+            if (cameraOffsetZ <= maxZoomInDistance)
+                transform.position = cameraStaticPosition + new Vector3(centerOfVectors.x, 0, maxZoomInDistance * (-1));
         }
         else
         {
-            if (cameraOffsetZ <= maxZoomDistance)
-                transform.position = new Vector3(transform.position.x, centerOfVectors.y, cameraOffsetZ * (-1));
+            if (cameraOffsetZ <= maxZoomOutDistance)
+                transform.position = new Vector3(transform.position.x, transform.position.y, cameraOffsetZ * (-1));
             else
-                transform.position = new Vector3(transform.position.x, centerOfVectors.y, maxZoomDistance * (-1));
+                transform.position = new Vector3(transform.position.x, transform.position.y, maxZoomOutDistance * (-1));
         }
 
     }
@@ -73,13 +81,14 @@ public class CameraObjectFollow : MonoBehaviour {
         return distances[index];
     }
 
-    Vector3 CenterOfVectors(List<GameObject> objects)
+    void CenterOfVectors(List<GameObject> objects)
     {
-        Vector3 sum = Vector3.zero;
+        minVector = Mathf.Infinity;
+        maxVector = -9999.0f;
         for (int i = 0; i < objects.Count; i++)
         {
-            sum += objects[i].transform.position;
+            minVector = (objects[i].transform.position.x < minVector) ? objects[i].transform.position.x : minVector;
+            maxVector = (objects[i].transform.position.x > maxVector) ? objects[i].transform.position.x : maxVector;
         }
-        return sum / objects.Count;
     }
 }
